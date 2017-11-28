b2a7a7fd9c714d975780051f108995b8a1357f5e
Suzal3579 <pe.messh@gmail.com>
2017-11-28 16:02:04 +0530

    ticking the time and testing it :D ... feeling good about it :D

--- app/components/CountdownComponent.jsx
+++ app/components/CountdownComponent.jsx
@@ -7,13 +7,32 @@ class CountdownComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            count: 0
+            count: 0,
+            countDownStatus: "stopped"
         };
         this.handleSetCountDown = this.handleSetCountDown.bind(this);
     }
+
+    // Gets after props or state is updated ...
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.countDownStatus !== prevState.countDownStatus) {
+            switch (this.state.countDownStatus) {
+            case "started":
+                this.startTimer();
+                break;
+            }
+        }
+    }
+    startTimer() {
+        this.timer = setInterval(() => {
+            const newCount = this.state.count - 1;
+            this.setState({ count: newCount >= 0 ? newCount : 0 });
+        }, 1000);
+    }
     handleSetCountDown(seconds) {
         this.setState({
-            count: seconds
+            count: seconds,
+            countDownStatus: "started"
         });
     }
     render() {
--- app/tests/components/Countdown.test.jsx
+++ app/tests/components/Countdown.test.jsx
@@ -5,19 +5,52 @@ import $ from "jQuery";
 import TestUtils from "react-dom/test-utils";
 
 import CountdownForm from "CountdownForm";
+import CountdownComponent from "CountdownComponent";
 
-describe("Count Down Form", () => {
-    it("should exist", () => {
+describe("Count Down Form ...", () => {
+    it("should exist - CountdownForm ...", () => {
+        // Checking if CountDownForm exists or not ...
         expect(CountdownForm).toBeTruthy();
     });
-    it("should call onSetDown using spy", () => {
-        const spy = expect.createSpy();
-        const countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy} />);
-        const $el = $(ReactDOM.findDOMNode(countdownForm));
-
-        countdownForm.refs.seconds.value = "109";
-        TestUtils.Simulate.submit($el.find("form")[0]);
-
-        expect(spy).toHaveBeenCalledWith(109);
+    it("should exist - CountdownComponent ... ", () => {
+        // Checking if CountdownComponent exists or not ...
+        expect(CountdownComponent).toBeTruthy();
     });
+
+    describe("HandleSetCountdown() testing ...", () => {
+        const countdown = TestUtils.renderIntoDocument(<CountdownComponent />);
+        countdown.handleSetCountDown(2);
+        it("should handle its work ...", (done) => {
+            expect(countdown.state.count).toBe(2);
+            expect(countdown.state.countDownStatus).toBe("started");
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(1);
+                // setInterval() is async in nature,
+                //  and by default mocha do not support this so,
+                //  done() is passed as an arguement in it() function 
+                // and is called after the process is done ...
+                done();
+            }, 1001);
+        });
+        it("should not go to negative numbers (timer zero vannda tala jana vayena !)...", (done) => {
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 4000);
+        })
+    });
+
+    // If interested use "jest" to use spies and do testing ... For expect 21+ createSpy is not working !!
+
+    // it("should call onSetDown using spy", () => {
+    //     const spy = expect.createSpy();
+    //     const countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy} />);
+    //     const $el = $(ReactDOM.findDOMNode(countdownForm));
+
+    //     countdownForm.refs.seconds.value = "109";
+    //     TestUtils.Simulate.submit($el.find("form")[0]);
+
+    //     expect(spy).toHaveBeenCalledWith(109);
+    // });
+
 });
