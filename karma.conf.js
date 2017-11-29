79309eef14fb357cfbca32245884395a1c154afa
Suzal3579 <pe.messh@gmail.com>
2017-11-29 22:38:12 +0530

    Karma conf fixes !!

--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,10 @@ module.exports = function (config) {
         frameworks: ["mocha"],
         browsers: ["Chrome"],
         singleRun: true,
-        files: ["app/tests/**/*.test.jsx"],
+        files: [
+            "node_modules/jquery/dist/jquery.min.js",
+            "node_modules/foundation-sites/dist/js/foundation.min.js",
+            "app/tests/**/*.test.jsx"],
         reporters: ["mocha"],
         preprocessors: {
             "app/tests/**/*.test.jsx": ["webpack", "sourcemap"]
